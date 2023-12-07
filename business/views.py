68fa1b19e9edd3e24830ff639105f332490d150f
ZaneFerns360 <zane.ferns360@gmail.com>
2023-12-07 20:00:44 +0530

    removed import

--- business/views.py
+++ business/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Business
 from django.utils import timezone
-from django import forms
 from django.forms import modelform_factory
 
 
